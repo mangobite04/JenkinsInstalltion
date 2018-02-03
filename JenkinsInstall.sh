9d85fb3541675046326bc997cae1b710019f30e6
mangobite04 <devesh.rai04@gmail.com>
2018-02-03 19:02:56 +0100

    Update JenkinsInstall.sh

--- JenkinsInstall.sh
+++ JenkinsInstall.sh
@@ -2,8 +2,6 @@
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
-systemctl start jenkins
-systemctl enable jenkins
 
 #Replace Jenkins Port
 sed -i 's/JENKINS_PORT=""/JENKINS_PORT="9090"/g' /etc/sysconfig/jenkins
@@ -13,3 +11,6 @@ java -jar jenkins.war --httpPort=9090
 
 #If you want to use https use the following command:
 java -jar jenkins.war --httpsPort=9090
+
+systemctl start jenkins
+systemctl enable jenkins
